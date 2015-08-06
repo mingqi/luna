4f0faf044361350c15fbfd5c83efb0fa003e47be
Mingqi Shao <shaomq@gmail.com>
2015-08-06 23:50:51 +0800

    fix a security bugs

--- bin/basic-iptables.sh
+++ bin/basic-iptables.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-iptables -D FORWARD -d 172.16.0.0/12 -i docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -D FORWARD -d 172.16.0.0/12 -i docker0 -j ALAUDA_LINK
-iptables -D FORWARD -d 172.16.0.0/12 -i docker0 -j DROP
+# FORWARD chain
+/sbin/iptables -D FORWARD -d 172.16.0.0/12 -i docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -D FORWARD -d 172.16.0.0/12 -i docker0 -j ALAUDA_LINK
+/sbin/iptables -D FORWARD -d 172.16.0.0/12 -i docker0 -j DROP
 
-iptables -N ALAUDA_LINK
+/sbin/iptables -N ALAUDA_LINK
 
-iptables -I FORWARD -d 172.16.0.0/12 -i docker0 -j DROP
-iptables -I FORWARD -d 172.16.0.0/12 -i docker0 -j ALAUDA_LINK
-iptables -I FORWARD -d 172.16.0.0/12 -i docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -I FORWARD -d 172.16.0.0/12 -i docker0 -j DROP
+/sbin/iptables -I FORWARD -d 172.16.0.0/12 -i docker0 -j ALAUDA_LINK
+/sbin/iptables -I FORWARD -d 172.16.0.0/12 -i docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# INPUT chain
+/sbin/iptables -D INPUT -d 172.16.0.0/12 -i docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -D INPUT -d 172.16.0.0/12 -i docker0 -j DROP
+
+/sbin/iptables -I INPUT -d 172.16.0.0/12 -i docker0 -j DROP
+/sbin/iptables -I INPUT -d 172.16.0.0/12 -i docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
