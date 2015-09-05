44e33774272c663fb32383971b03d37bf9e9b1e3
Mingqi Shao <shaomq@gmail.com>
2015-09-05 12:02:49 +0800

    add oversell feature support

---
+++ luna/oversell.py
@@ -0,0 +1,20 @@
+from luna import docker_cli_parser
+
+
+class Run(object):
+
+    def __init__(self, args):
+        self.args = args
+        opts, args = docker_cli_parser.run(args)
+        self.mem_oversell_rate = 1
+        for env in opts.get('env', []):
+            name, value = env.split('=')
+            if name == '__ALAUDA_MEM_OVERSELL_RATE__':
+                self.mem_oversell_rate = float(value)
+        self.memory = int(opts.get('memory', None))
+
+    def to_args(self):
+        if self.memory:
+            self.memory = int(self.memory * self.mem_oversell_rate)
+            self.args[self.args.index('-m') + 1] = str(self.memory)
+        return self.args
