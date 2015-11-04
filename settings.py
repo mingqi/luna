a4899ab3762e1b04f35844652cfb05af50f27538
Mingqi Shao <shaomq@gmail.com>
2015-11-04 17:30:17 +0800

    logging handler is configurable

--- settings.py
+++ settings.py
@@ -11,36 +11,4 @@ JAKIRO = {
 DOCKER_API_VERSION = '1.17'
 LINA_ENDPOINT = "lina.int.alauda.club:8080"
 DNS = ['8.8.8.8', '4.4.4.4']
-LOGGING = {
-    'version': 1,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)-15s %(name)s [%(levelname)s]: %(message)s'
-        }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
-            'formatter': 'standard',
-            'stream': sys.stdout
-        },
-        'file': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'standard',
-            'filename': '/var/log/mathilde/lina.log',
-            'maxBytes': 1024 * 1024 * 50,
-            'backupCount': 5
-        }
-    },
-    'loggers': {
-        'peewee': {
-            'level': 'INFO',
-            'propagate': 1
-        }
-    },
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['console', 'file']
-    }
-}
+LOGGING_HANDLERS = ['console', 'file']
